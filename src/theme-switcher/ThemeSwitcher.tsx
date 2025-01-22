31dea32b9cf61410936bd45c9b076cd2bb22bc65
arrKhange1 <therock65frostik@gmail.com>
2025-01-22 18:26:57 +0500

    fix: imgs imports

--- src/theme-switcher/ThemeSwitcher.tsx
+++ src/theme-switcher/ThemeSwitcher.tsx
@@ -1,6 +1,8 @@
 import { useContext } from 'react';
 import { ThemeContext } from '../app/App';
 import cls from './ThemeSwitcher.module.css';
+import lightImg from '../ui/icons/light.svg';
+import darkImg from '../ui/icons/dark.svg';
 
 interface ThemeSwitcherProps {
   toggleTheme: () => void;
@@ -10,8 +12,8 @@ export const ThemeSwitcher: React.FC<ThemeSwitcherProps> = ({ toggleTheme }) => 
   const isLight = useContext<boolean>(ThemeContext);
   return (
     <button className={cls.switcher} onClick={() => toggleTheme()}>
-      {isLight && <img className={cls.switcherIcon} src="src/ui/icons/dark.svg" />}
-      {!isLight && <img className={cls.switcherIcon} src="src/ui/icons/light.svg" />}
+      {isLight && <img className={cls.switcherIcon} src={lightImg} />}
+      {!isLight && <img className={cls.switcherIcon} src={darkImg} />}
     </button>
   );
 };
